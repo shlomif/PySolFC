e9d04fcf4b3d4bb4d554d93e08ca16ec1b0992f3
Shlomi Fish <shlomif@shlomifish.org>
2018-03-19 16:54:13 +0200

    Convert to unittest.py.

--- tests/unit/acard_unit.py
+++ tests/unit/acard_unit.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python3
 # Written by Shlomi Fish, under the MIT Expat License.
 
-# imports
-import sys
-from TAP.Simple import plan, ok
-
+import unittest
 from pysollib.acard import AbstractCard
 
 
-def shlomif_main(args):
+class MyTests(unittest.TestCase):
+    def test_cards(self):
+        card1 = AbstractCard(1001, 0, 1, 2, 3001)
+        # TEST
+        self.assertEqual(card1.color, 0, 'card1.color is sane.')
 
-    plan(4)
+        # TEST
+        self.assertEqual(card1.rank, 2, 'card1.rank')
 
-    card1 = AbstractCard(1001, 0, 1, 2, 3001)
-    # TEST
-    ok(card1.color == 0, 'card1.color is sane.')
+        card2 = AbstractCard(1001, 0, 3, 7, 3001)
+        # TEST
+        self.assertEqual(card2.color, 1, 'card2.color is sane.')
 
-    # TEST
-    ok(card1.rank == 2, 'card1.rank')
-
-    card2 = AbstractCard(1001, 0, 3, 7, 3001)
-    # TEST
-    ok(card2.color == 1, 'card2.color is sane.')
-
-    # TEST
-    ok(card2.rank == 7, 'card2.rank')
+        # TEST
+        self.assertEqual(card2.rank, 7, 'card2.rank')
 
 
-if __name__ == "__main__":
-    sys.exit(shlomif_main(sys.argv))
+if __name__ == '__main__':
+    from pycotap import TAPTestRunner
+    suite = unittest.TestLoader().loadTestsFromTestCase(MyTests)
+    TAPTestRunner().run(suite)

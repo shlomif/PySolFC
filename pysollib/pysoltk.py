16d8b00e7f26b150a67bf70ebb1fe05008872fcc
Shlomi Fish <shlomif@shlomifish.org>
2018-03-19 19:17:33 +0200

    flake8

--- pysollib/pysoltk.py
+++ pysollib/pysoltk.py
@@ -24,94 +24,94 @@
 from pysollib.settings import TOOLKIT, USE_TILE
 
 if TOOLKIT == 'tk':
-    from pysollib.ui.tktile.tkconst import *
-    from pysollib.ui.tktile.tkutil import *
-    from pysollib.ui.tktile.card import *
-    from pysollib.ui.tktile.tkcanvas import *
-    from pysollib.ui.tktile.tkwrap import *
-    from pysollib.ui.tktile.findcarddialog import *
+    from pysollib.ui.tktile.tkconst import *  # noqa: F401,F403
+    from pysollib.ui.tktile.tkutil import *  # noqa: F401,F403
+    from pysollib.ui.tktile.card import *  # noqa: F401,F403
+    from pysollib.ui.tktile.tkcanvas import *  # noqa: F401,F403
+    from pysollib.ui.tktile.tkwrap import *  # noqa: F401,F403
+    from pysollib.ui.tktile.findcarddialog import *  # noqa: F401,F403
     if USE_TILE:
-        from pysollib.tile.tkwidget import *
-        from pysollib.tile.tkhtml import *
-        from pysollib.tile.edittextdialog import *
-        from pysollib.tile.tkstats import *
-        from pysollib.tile.playeroptionsdialog import *
-        from pysollib.tile.soundoptionsdialog import *
-        from pysollib.tile.timeoutsdialog import *
-        from pysollib.tile.colorsdialog import *
-        from pysollib.tile.fontsdialog import *
-        from pysollib.tile.solverdialog import *
-        from pysollib.tile.gameinfodialog import *
-        from pysollib.tile.toolbar import *
-        from pysollib.tile.statusbar import *
-        from pysollib.tile.progressbar import *
-        from pysollib.tile.menubar import *
-        from pysollib.tile.selectcardset import *
-        from pysollib.tile.selecttree import *
+        from pysollib.tile.tkwidget import *  # noqa: F401,F403
+        from pysollib.tile.tkhtml import *  # noqa: F401,F403
+        from pysollib.tile.edittextdialog import *  # noqa: F401,F403
+        from pysollib.tile.tkstats import *  # noqa: F401,F403
+        from pysollib.tile.playeroptionsdialog import *  # noqa: F401,F403
+        from pysollib.tile.soundoptionsdialog import *  # noqa: F401,F403
+        from pysollib.tile.timeoutsdialog import *  # noqa: F401,F403
+        from pysollib.tile.colorsdialog import *  # noqa: F401,F403
+        from pysollib.tile.fontsdialog import *  # noqa: F401,F403
+        from pysollib.tile.solverdialog import *  # noqa: F401,F403
+        from pysollib.tile.gameinfodialog import *  # noqa: F401,F403
+        from pysollib.tile.toolbar import *  # noqa: F401,F403
+        from pysollib.tile.statusbar import *  # noqa: F401,F403
+        from pysollib.tile.progressbar import *  # noqa: F401,F403
+        from pysollib.tile.menubar import *  # noqa: F401,F403
+        from pysollib.tile.selectcardset import *  # noqa: F401,F403
+        from pysollib.tile.selecttree import *  # noqa: F401,F403
     else:
-        from pysollib.tk.tkwidget import *
-        from pysollib.tk.tkhtml import *
-        from pysollib.tk.edittextdialog import *
-        from pysollib.tk.tkstats import *
-        from pysollib.tk.playeroptionsdialog import *
-        from pysollib.tk.soundoptionsdialog import *
-        from pysollib.tk.timeoutsdialog import *
-        from pysollib.tk.colorsdialog import *
-        from pysollib.tk.fontsdialog import *
-        from pysollib.tk.solverdialog import *
-        from pysollib.tk.gameinfodialog import *
-        from pysollib.tk.toolbar import *
-        from pysollib.tk.statusbar import *
-        from pysollib.tk.progressbar import *
-        from pysollib.tk.menubar import *
-        from pysollib.tk.selectcardset import *
-        from pysollib.tk.selecttree import *
+        from pysollib.tk.tkwidget import *  # noqa: F401,F403
+        from pysollib.tk.tkhtml import *  # noqa: F401,F403
+        from pysollib.tk.edittextdialog import *  # noqa: F401,F403
+        from pysollib.tk.tkstats import *  # noqa: F401,F403
+        from pysollib.tk.playeroptionsdialog import *  # noqa: F401,F403
+        from pysollib.tk.soundoptionsdialog import *  # noqa: F401,F403
+        from pysollib.tk.timeoutsdialog import *  # noqa: F401,F403
+        from pysollib.tk.colorsdialog import *  # noqa: F401,F403
+        from pysollib.tk.fontsdialog import *  # noqa: F401,F403
+        from pysollib.tk.solverdialog import *  # noqa: F401,F403
+        from pysollib.tk.gameinfodialog import *  # noqa: F401,F403
+        from pysollib.tk.toolbar import *  # noqa: F401,F403
+        from pysollib.tk.statusbar import *  # noqa: F401,F403
+        from pysollib.tk.progressbar import *  # noqa: F401,F403
+        from pysollib.tk.menubar import *  # noqa: F401,F403
+        from pysollib.tk.selectcardset import *  # noqa: F401,F403
+        from pysollib.tk.selecttree import *  # noqa: F401,F403
 
-elif TOOLKIT=='kivy':
-     from pysollib.kivy.tkconst import *
-     from pysollib.kivy.tkutil import *
-     from pysollib.kivy.card import *
-     from pysollib.kivy.tkcanvas import *
-     from pysollib.kivy.tkwrap import *
-     from pysollib.kivy.findcarddialog import *
-     from pysollib.kivy.tkwidget import *
-     from pysollib.kivy.tkhtml import *
-     from pysollib.kivy.edittextdialog import *
-     from pysollib.kivy.tkstats import *
-     from pysollib.kivy.playeroptionsdialog import *
-     #from pysollib.kivy.soundoptionsdialog import *
-     from pysollib.kivy.timeoutsdialog import *
-     from pysollib.kivy.colorsdialog import *
-     from pysollib.kivy.fontsdialog import *
-     from pysollib.kivy.solverdialog import *
-     from pysollib.kivy.gameinfodialog import *
-     from pysollib.kivy.toolbar import *
-     from pysollib.kivy.statusbar import *
-     from pysollib.kivy.progressbar import *
-     from pysollib.kivy.menubar import *
-     from pysollib.kivy.selectcardset import *
-     from pysollib.kivy.selecttree import *
+elif TOOLKIT == 'kivy':
+    from pysollib.kivy.tkconst import *  # noqa: F401,F403
+    from pysollib.kivy.tkutil import *  # noqa: F401,F403
+    from pysollib.kivy.card import *  # noqa: F401,F403
+    from pysollib.kivy.tkcanvas import *  # noqa: F401,F403
+    from pysollib.kivy.tkwrap import *  # noqa: F401,F403
+    from pysollib.kivy.findcarddialog import *  # noqa: F401,F403
+    from pysollib.kivy.tkwidget import *  # noqa: F401,F403
+    from pysollib.kivy.tkhtml import *  # noqa: F401,F403
+    from pysollib.kivy.edittextdialog import *  # noqa: F401,F403
+    from pysollib.kivy.tkstats import *  # noqa: F401,F403
+    from pysollib.kivy.playeroptionsdialog import *  # noqa: F401,F403
+    # from pysollib.kivy.soundoptionsdialog import *  # noqa: F401,F403
+    from pysollib.kivy.timeoutsdialog import *  # noqa: F401,F403
+    from pysollib.kivy.colorsdialog import *  # noqa: F401,F403
+    from pysollib.kivy.fontsdialog import *  # noqa: F401,F403
+    from pysollib.kivy.solverdialog import *  # noqa: F401,F403
+    from pysollib.kivy.gameinfodialog import *  # noqa: F401,F403
+    from pysollib.kivy.toolbar import *  # noqa: F401,F403
+    from pysollib.kivy.statusbar import *  # noqa: F401,F403
+    from pysollib.kivy.progressbar import *  # noqa: F401,F403
+    from pysollib.kivy.menubar import *  # noqa: F401,F403
+    from pysollib.kivy.selectcardset import *  # noqa: F401,F403
+    from pysollib.kivy.selecttree import *  # noqa: F401,F403
 
-else: # gtk
-    from pysollib.pysolgtk.tkconst import *
-    from pysollib.pysolgtk.tkutil import *
-    from pysollib.pysolgtk.tkcanvas import *
-    from pysollib.pysolgtk.tkwrap import *
-    from pysollib.pysolgtk.tkwidget import *
-    from pysollib.pysolgtk.tkhtml import *
-    from pysollib.pysolgtk.edittextdialog import *
-    from pysollib.pysolgtk.tkstats import *
-    from pysollib.pysolgtk.playeroptionsdialog import *
-    from pysollib.pysolgtk.soundoptionsdialog import *
-    from pysollib.pysolgtk.timeoutsdialog import *
-    from pysollib.pysolgtk.colorsdialog import *
-    from pysollib.pysolgtk.fontsdialog import *
-    from pysollib.pysolgtk.findcarddialog import *
-    from pysollib.pysolgtk.solverdialog import *
-    from pysollib.pysolgtk.gameinfodialog import *
-    from pysollib.pysolgtk.toolbar import *
-    from pysollib.pysolgtk.statusbar import *
-    from pysollib.pysolgtk.progressbar import *
-    from pysollib.pysolgtk.menubar import *
-    from pysollib.pysolgtk.card import *
-    from pysollib.pysolgtk.selectcardset import *
+else:  # gtk
+    from pysollib.pysolgtk.tkconst import *  # noqa: F401,F403
+    from pysollib.pysolgtk.tkutil import *  # noqa: F401,F403
+    from pysollib.pysolgtk.tkcanvas import *  # noqa: F401,F403
+    from pysollib.pysolgtk.tkwrap import *  # noqa: F401,F403
+    from pysollib.pysolgtk.tkwidget import *  # noqa: F401,F403
+    from pysollib.pysolgtk.tkhtml import *  # noqa: F401,F403
+    from pysollib.pysolgtk.edittextdialog import *  # noqa: F401,F403
+    from pysollib.pysolgtk.tkstats import *  # noqa: F401,F403
+    from pysollib.pysolgtk.playeroptionsdialog import *  # noqa: F401,F403
+    from pysollib.pysolgtk.soundoptionsdialog import *  # noqa: F401,F403
+    from pysollib.pysolgtk.timeoutsdialog import *  # noqa: F401,F403
+    from pysollib.pysolgtk.colorsdialog import *  # noqa: F401,F403
+    from pysollib.pysolgtk.fontsdialog import *  # noqa: F401,F403
+    from pysollib.pysolgtk.findcarddialog import *  # noqa: F401,F403
+    from pysollib.pysolgtk.solverdialog import *  # noqa: F401,F403
+    from pysollib.pysolgtk.gameinfodialog import *  # noqa: F401,F403
+    from pysollib.pysolgtk.toolbar import *  # noqa: F401,F403
+    from pysollib.pysolgtk.statusbar import *  # noqa: F401,F403
+    from pysollib.pysolgtk.progressbar import *  # noqa: F401,F403
+    from pysollib.pysolgtk.menubar import *  # noqa: F401,F403
+    from pysollib.pysolgtk.card import *  # noqa: F401,F403
+    from pysollib.pysolgtk.selectcardset import *  # noqa: F401,F403

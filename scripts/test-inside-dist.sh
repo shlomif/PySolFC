5c663272c5cd2b35f81ce70b899f6d31f2f42f70
Shlomi Fish <shlomif@shlomifish.org>
2018-05-09 18:06:17 +0300

    Add the first ver of the script.

---
+++ scripts/test-inside-dist.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+#
+# y.sh
+# Copyright (C) 2018 shlomif <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+rm -fr PySolFC-2.2.0
+make dist
+rm -fr PySolFC-2.2.0
+tar -xvf dist/PySolFC-2.2.0.tar.xz
+(cd PySolFC-2.2.0 && make test)

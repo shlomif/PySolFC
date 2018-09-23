8d208f8ed6b3eb5892cbf194c842088630883fc4
Shlomi Fish <shlomif@shlomifish.org>
2018-09-23 11:16:54 +0300

    Add a repack script for the cardsets.
    See https://github.com/shlomif/PySolFC/issues/89 .

---
+++ scripts/repack-min-cardsets.bash
@@ -0,0 +1,30 @@
+#! /bin/bash
+#
+# repack-min-cardsets.bash
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+set -e -x
+
+src_base="PySolFC-Cardsets"
+dest_base="$src_base--Minimal"
+ver="2.0"
+src_vbase="$src_base-2.0"
+dest_vbase="$dest_base-2.0"
+src_arc="$src_vbase.tar.bz2"
+
+if ! test -f "$src_arc"
+then
+    wget -c "https://sourceforge.net/projects/pysolfc/files/$src_base/$src_vbase/$src_arc/download" -O "$src_arc"
+fi
+
+tar -xvf "$src_arc"
+mkdir -p "$dest_vbase"
+cat scripts/cardsets_to_bundle | (while read b
+do
+    cp -a "$src_vbase/$b" "$dest_vbase/$b"
+done)
+
+tar -cavf "$dest_vbase.tar.xz" "$dest_vbase"

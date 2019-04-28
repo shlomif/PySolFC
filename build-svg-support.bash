d48b2027b5f808aa48360ef6c901594869468d2e
Shlomi Fish <shlomif@shlomifish.org>
2019-04-28 20:46:05 +0300

    add script

---
+++ build-svg-support.bash
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+#
+# build-svg-support.bash
+# Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+set -e -x
+(
+a="`pwd`"
+cd kcardgame
+git clone git://anongit.kde.org/kpat
+mkdir build-kpat/
+k="`pwd`"
+cd build-kpat
+cmake -DCMAKE_BUILD_TYPE=Debug ../kpat
+make
+cd ..
+mkdir b
+cd b
+cmake ../binding-example/
+make
+) || true
+export PYTHONPATH=$PWD/kcardgame/b

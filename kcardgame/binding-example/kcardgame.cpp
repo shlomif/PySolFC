487d646cce038f586f2fdfb585bd2855326fbf44
Shlomi Fish <shlomif@shlomifish.org>
2019-04-28 20:34:28 +0300

    got the svg cards to work as a PoC.
    See https://github.com/shlomif/PySolFC/issues/3 .

--- kcardgame/binding-example/kcardgame.cpp
+++ kcardgame/binding-example/kcardgame.cpp
@@ -6,7 +6,16 @@ MyKCardDeck::MyKCardDeck()
         int argc = 1;
         char * argv[2]={"pysol",NULL};
         app = new QGuiApplication(argc, argv);
-    d = new KCardDeck( KCardTheme(), nullptr);
+    d = new KCardDeck( KCardTheme("svg-ancient-egyptians"), nullptr);
+    unsigned int number = 0;
+    QList<quint32> ids;
+    for ( int i = 0; i < 1; ++i )
+        foreach ( const KCardDeck::Rank & r, KCardDeck::standardRanks() )
+            foreach ( const KCardDeck::Suit & s, KCardDeck::standardSuits() )
+                ids << KCardDeck::getId( s, r, number++ );
+
+    d->setDeckContents(ids);
+    d->setCardWidth(150);
     }
 QPixmap *MyKCardDeck::get_card_pixmap(int i)
 {

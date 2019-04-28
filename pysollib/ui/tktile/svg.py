3ca42f2b263b1a9a8a6b242383b0dd3820a13598
Shlomi Fish <shlomif@shlomifish.org>
2019-04-28 03:04:39 +0300

    got tiny images to show.

--- pysollib/ui/tktile/svg.py
+++ pysollib/ui/tktile/svg.py
@@ -7,9 +7,14 @@
 
 import cairo
 
-from gi.repository import Rsvg
+from gi import require_version
+require_version('Rsvg', '2.0')
+from gi.repository import Rsvg  # noqa: E402
 
-from pysollib.mfxutil import Image, ImageTk
+import pysnooper  # noqa: E402
+
+
+from pysollib.mfxutil import Image  # noqa: E402
 
 
 class SVGManager:
@@ -18,19 +23,35 @@ class SVGManager:
         self.filename = filename
         self.svg = Rsvg.Handle().new_from_file(filename)
 
+    # Taken from https://stackoverflow.com/questions/44471795
+    # Under MIT License - thanks.
+    def pixbuf2image(self, pxb):
+        """ Convert GdkPixbuf.Pixbuf to PIL image """
+        data = pxb.get_pixels()
+        w = pxb.get_width()
+        h = pxb.get_height()
+        stride = pxb.get_rowstride()
+        mode = "RGB"
+        if pxb.get_has_alpha():
+            mode = "RGBA"
+        img = Image.frombytes(mode, (w, h), data, "raw", mode, stride)
+        return img
+
+    @pysnooper.snoop()
     def render_fragment(self, id_, width, height):
         id__ = '#' + id_
         """docstring for render_"""
         dims = self.svg.get_dimensions_sub(id__)[1]
         width_, height_ = dims.width, dims.height
-        surface = cairo.ImageSurface(cairo.FORMAT_ARGB32,
-                                     int(width_), int(height_))
-        context = cairo.Context(surface)
-        self.svg.render_cairo_sub(context, id__)
-        tk_image = ImageTk.PhotoImage('RGBA')
-        image = Image.frombuffer('RGBA', (width_, height_),
-                                 bytes(surface.get_data()),
-                                 'raw', 'BGRA', 0, 1)
-        return image.crop((0, 0, width, height))
-        tk_image = ImageTk.PhotoImage(image.crop((0, 0, width, height)))
-        return tk_image
+        pix = self.svg.get_pixbuf_sub(id__)
+        if False:
+            surface = cairo.ImageSurface(cairo.FORMAT_ARGB32,
+                                         int(width_), int(height_))
+            context = cairo.Context(surface)
+            # context.scale(width_, height_)
+            assert self.svg.render_cairo_sub(context, id__)
+            buf = bytes(surface.get_data())
+            image = Image.frombuffer('RGBA', (width_, height_), buf,
+                                     'raw', 'BGRA', 0, 1)
+        image = self.pixbuf2image(pix)
+        return image.resize((width, height))

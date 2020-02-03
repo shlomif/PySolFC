f96ceb57a064267f8b2da4738b515c36efe9d858
Juhani Numminen <juhaninumminen0@gmail.com>
2020-02-03 10:56:59 +0200

    install-pysolfc.sh: Use bash array for holding a program name and arguments
    The previous scalar variable with quoted "$PIP" caused a "command not found"
    error because that erroneously put prog name and arguments in a single token.

--- contrib/install-pysolfc.sh
+++ contrib/install-pysolfc.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 set -Cefu
 
 : ${PKGTREE:=/usr/local/packages/PySolFC}
-PIP="$(printf '%s/env/bin/pip install --no-binary :all: ' "$PKGTREE")"
+PIP=("${PKGTREE}/env/bin/pip" install --no-binary :all:)
 PYPROG="$(printf '%s/env/bin/python' "$PKGTREE")"
 VERSION="$(env PYTHONPATH=`pwd` "$PYPROG" -c 'from pysollib.settings import VERSION ; print(VERSION)')"
 XZBALL="$(printf 'dist/PySolFC-%s.tar.xz' "$VERSION")"
@@ -11,6 +11,6 @@ reqs='six random2 pillow'
 make dist
 for req in $reqs
 do
-    "$PIP" "$req"
+    "${PIP[@]}" "$req"
 done
-"$PIP" --upgrade "$XZBALL"
+"${PIP[@]}" --upgrade "$XZBALL"

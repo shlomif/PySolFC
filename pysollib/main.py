shlomif/PySolFC

commit 65b86f2d1d6f53fef133af897c673568205ce7a3

Improve internationalization
- More strings marked for translation
- Used format strings instead of concatinating fragments
- Removed space before '?' or '!' from sentences in English

Following the GNU gettext guidelines at
https://www.gnu.org/software/gettext/manual/gettext.html#Preparing-Strings

pysollib/main.py
@@ -56,25 +56,25 @@ if TOOLKIT == 'kivy':
     def fatal_no_cardsets(app):
         app.wm_withdraw()
         MfxMessageDialog(app.top, title=_("%s installation error") % TITLE,
-                         text=_('''No cardsets were found !!!
+                         text=_('''No cardsets were found!!!
 
 Cardsets should be installed into:
-%s/cardsets/
+%(dir)s
 
-Please check your %s installation.
-''') % (getprefdir(PACKAGE), TITLE),
+Please check your %(app)s installation.
+''') % {'dir': getprefdir(PACKAGE) + '/cardsets/', 'app': TITLE},
             bitmap="error", strings=(_("&Quit"),))
 else:
     def fatal_no_cardsets(app):
         app.wm_withdraw()
         MfxMessageDialog(app.top, title=_("%s installation error") % TITLE,
-                         text=_('''No cardsets were found !!!
+                         text=_('''No cardsets were found!!!
 
 Main data directory is:
-%s
+%(dir)s
 
-Please check your %s installation.
-''') % (app.dataloader.dir, TITLE),
+Please check your %(app)s installation.
+''') % {'dir': app.dataloader.dir, 'app': TITLE},
                      bitmap="error", strings=(_("&Quit"),))
 
 
@@ -93,8 +93,8 @@ def parse_option(argv):
                                        "sound-mod=",
                                        "help"])
     except getopt.GetoptError as err:
-        print_err(_("%s\ntry %s --help for more information") %
-                  (err, prog_name), 0)
+        print_err(err + "\n" + _("try %s --help for more information") %
+                  prog_name, 0)
         return None
     opts = {"help": False,
             "deal": None,
@@ -303,13 +303,14 @@ def pysol_init(app, args):
         app.intro.progress.destroy()
         d = MfxMessageDialog(top, title=_("%s installation error") % TITLE,
                              text=_('''
-No games were found !!!
+No games were found!!!
 
 Main data directory is:
-%s
+%(dir)s
 
-Please check your %s installation.
-''') % (app.dataloader.dir, TITLE), bitmap="error", strings=(_("&Quit"),))
+Please check your %(app)s installation.
+''') % {'dir': app.dataloader.dir, 'app': TITLE},
+                             bitmap="error", strings=(_("&Quit"),))
         return 1
 
     # init cardsets

Makefile
@@ -40,7 +40,7 @@ rules:
 
 pot:
 	./scripts/all_games.py gettext > po/games.pot
-	xgettext --keyword=n_ -o po/pysol.pot \
+	xgettext --keyword=n_ --add-comments=TRANSLATORS: -o po/pysol.pot \
 		pysollib/*.py pysollib/*/*.py pysollib/*/*/*.py data/pysolfc.glade
 	set -e; \
 	for lng in ru de pl it; do \

pysollib/actions.py
@@ -47,7 +47,7 @@ from pysollib.pysoltk import create_find_card_dialog
 from pysollib.pysoltk import create_solver_dialog
 from pysollib.settings import DEBUG
 from pysollib.settings import PACKAGE_URL, TITLE
-from pysollib.settings import TOP_TITLE
+from pysollib.settings import TOP_SIZE
 from pysollib.stats import FileStatsFormatter
 
 
@@ -381,7 +381,7 @@ class PysolMenubar(PysolMenubarTk):
         if self._cancelDrag():
             return
         if self.changed():
-            if not self.game.areYouSure(_("Quit ") + TITLE):
+            if not self.game.areYouSure(_("Quit %s") % TITLE):
                 return
         self.game.endGame()
         self.game.quitGame()
@@ -445,7 +445,7 @@ class PysolMenubar(PysolMenubarTk):
         if not self.game.gsaveinfo.bookmarks:
             return
         if not self.game.areYouSure(_("Clear bookmarks"),
-                                    _("Clear all bookmarks ?")):
+                                    _("Clear all bookmarks?")):
             return
         self.game.gsaveinfo.bookmarks = {}
         self.game.updateMenus()
@@ -457,7 +457,7 @@ class PysolMenubar(PysolMenubarTk):
             return
         if self.changed(restart=1):
             if not self.game.areYouSure(_("Restart game"),
-                                        _("Restart this game ?")):
+                                        _("Restart this game?")):
                 return
         self.game.restartGame()
 
@@ -501,10 +501,11 @@ class PysolMenubar(PysolMenubarTk):
         if self._cancelDrag(break_pause=False):
             return
         game, gi = self.game, self.game.gameinfo
-        t = " " + game.getGameNumber(format=1)
-        cc = _("Comments for %s:\n\n") % (gi.name + t)
+        kw = {'game': gi.name,
+              'id': game.getGameNumber(format=1)}
+        cc = _("Comments for %(game)s %(id)s:\n\n") % kw
         c = game.gsaveinfo.comment or cc
-        d = EditTextDialog(game.top, _("Comments for ")+t, text=c)
+        d = EditTextDialog(game.top, _("Comments for %(id)s") % kw, text=c)
         if d.status == 0 and d.button == 0:
             text = d.text
             if text.strip() == cc.strip():
@@ -525,8 +526,9 @@ class PysolMenubar(PysolMenubarTk):
                         text=_("Error while writing to file"))
                 else:
                     d = MfxMessageDialog(
-                        self.top, title=TITLE+_(" Info"), bitmap="info",
-                        text=_("Comments were appended to\n\n") + fn)
+                        self.top, title=_("%s Info") % TITLE, bitmap="info",
+                        text=_("Comments were appended to\n\n%(filename)s")
+                        % {'filename': fn})
         self._setCommentMenu(bool(game.gsaveinfo.comment))
 
     #
@@ -535,10 +537,10 @@ class PysolMenubar(PysolMenubarTk):
 
     def _mStatsSave(self, player, filename, write_method):
         if player is None:
-            text = _("Demo statistics")
+            text = _("Demo statistics were appended to\n\n%(filename)s")
             filename = filename + "_demo"
         else:
-            text = _("Your statistics")
+            text = _("Your statistics were appended to\n\n%(filename)s")
         filename = os.path.join(self.app.dn.config, filename + ".txt")
         filename = os.path.normpath(filename)
         try:
@@ -549,8 +551,8 @@ class PysolMenubar(PysolMenubarTk):
                                text=_("Error while writing to file"))
         else:
             MfxMessageDialog(
-                self.top, title=TITLE+_(" Info"), bitmap="info",
-                text=text + _(" were appended to\n\n") + filename)
+                self.top, title=_("%s Info") % TITLE, bitmap="info",
+                text=text % {'filename': filename})
 
     def mPlayerStats(self, *args, **kw):
         mode = kw.get("mode", 101)
@@ -564,31 +566,40 @@ class PysolMenubar(PysolMenubarTk):
             d = Struct(status=-1, button=-1)
             if demo:
                 player = None
-                p0, p1, p2 = TITLE+_(" Demo"), TITLE+_(" Demo "), ""
             else:
                 player = self.app.opt.player
-                p0, p1, p2 = player, "", _(" for ") + player
             n = self.game.gameinfo.name
+            # translation keywords
+            transkw = {'app': TITLE,
+                       'player': player,
+                       'game': n,
+                       'tops': TOP_SIZE}
             #
             if mode == 100:
                 d = Status_StatsDialog(self.top, game=self.game)
             elif mode == 101:
-                header = p1 + _("Statistics for ") + n
+                header = (_("%(app)s Demo Statistics for %(game)s") if demo
+                          else _("Statistics for %(game)s")) % transkw
                 d = SingleGame_StatsDialog(
                    self.top, header, self.app, player, gameid=self.game.id)
                 gameid = d.selected_game
             elif mode == 102:
-                header = p1 + _("Statistics") + p2
+                header = (_("%(app)s Demo Statistics") if demo
+                          else _("Statistics for %(player)s")) % transkw
                 d = AllGames_StatsDialog(self.top, header, self.app, player)
                 gameid = d.selected_game
             elif mode == 103:
-                header = p1 + _("Full log") + p2
+                header = (_("%(app)s Demo Full log") if demo
+                          else _("Full log for %(player)s")) % transkw
                 d = FullLog_StatsDialog(self.top, header, self.app, player)
             elif mode == 104:
-                header = p1 + _("Session log") + p2
+                header = (_("%(app)s Demo Session log") if demo
+                          else _("Session log for %(player)s")) % transkw
                 d = SessionLog_StatsDialog(self.top, header, self.app, player)
             elif mode == 105:
-                header = p1 + TOP_TITLE + _(" for ") + n
+                # TRANSLATORS: eg. top 10 or top 5 results for a certain game
+                header = (_("%(app)s Demo Top %(tops)d for %(game)s") if demo
+                          else _("Top %(tops)d for %(game)s")) % transkw
                 d = Top_StatsDialog(
                     self.top, header, self.app, player, gameid=self.game.id)
             elif mode == 106:
@@ -614,7 +625,8 @@ class PysolMenubar(PysolMenubarTk):
                 # reset all player stats
                 if self.game.areYouSure(
                     _("Reset all statistics"),
-                    _("Reset ALL statistics and logs for player\n%s ?") % p0,
+                    _("Reset ALL statistics and logs for player\n" +
+                      "%(player)s?") % transkw,
                     confirm=1, default=1
                 ):
                     self.app.stats.resetStats(player, 0)
@@ -624,8 +636,8 @@ class PysolMenubar(PysolMenubarTk):
                 # reset player stats for current game
                 if self.game.areYouSure(
                     _("Reset game statistics"),
-                    _('Reset statistics and logs ' +
-                        'for player\n%s\nand game\n%s ?') % (p0, n),
+                    _('Reset statistics and logs for player\n%(player)s\n'
+                      'and game\n%(game)s?') % transkw,
                     confirm=1, default=1
                 ):
                     self.app.stats.resetStats(player, self.game.id)

pysollib/app.py
@@ -60,7 +60,7 @@ from pysollib.resource import Tile, TileManager
 from pysollib.settings import DEBUG
 from pysollib.settings import PACKAGE, VERSION_TUPLE, WIN_SYSTEM
 from pysollib.settings import TOOLKIT
-from pysollib.util import CARDSET, IMAGE_EXTENSIONS
+from pysollib.util import IMAGE_EXTENSIONS
 from pysollib.winsystems import TkSettings
 if TOOLKIT == 'tk':
     from pysollib.ui.tktile.solverdialog import destroy_solver_dialog
@@ -329,7 +329,8 @@ class Application:
             elif self.commandline.game is not None:
                 gameid = self.gdb.getGameByName(self.commandline.game)
                 if gameid is None:
-                    print_err(_("can't find game: ") + self.commandline.game)
+                    print_err(_("can't find game: %(game)s") % {
+                        'game': self.commandline.game})
                     sys.exit(-1)
                 else:
                     self.nextgame.id = gameid
@@ -668,7 +669,7 @@ class Application:
         if progress is None:
             self.wm_save_state()
             self.wm_withdraw()
-            title = _("Loading %s %s...") % (CARDSET, cs.name)
+            title = _("Loading cardset %s...") % cs.name
             color = self.opt.colors['table']
             if self.tabletile_index > 0:
                 color = "#008200"
@@ -678,7 +679,7 @@ class Application:
         images = Images(self.dataloader, cs)
         try:
             if not images.load(app=self, progress=progress):
-                raise Exception("Invalid or damaged "+CARDSET)
+                raise Exception("Invalid or damaged cardset")
             simages = SubsampledImages(images)
             if self.opt.save_cardsets:
                 c = self.cardsets_cache.get(cs.type)
@@ -710,8 +711,8 @@ class Application:
             # images.destruct()
             destruct(images)
             MfxExceptionDialog(
-                self.top, ex, title=CARDSET+_(" load error"),
-                text=_("Error while loading ")+CARDSET)
+                self.top, ex, title=_("Cardset load error"),
+                text=_("Error while loading cardset"))
         self.intro.progress = progress
         if r and self.menubar is not None:
             self.menubar.updateBackgroundImagesMenu()
@@ -806,14 +807,14 @@ class Application:
         #
         t = self.checkCompatibleCardsetType(gi, self.cardset)
         MfxMessageDialog(
-            self.top, title=_("Incompatible ")+CARDSET,
+            self.top, title=_("Incompatible cardset"),
             bitmap="warning",
-            text=_('''The currently selected %s %s
+            text=_('''The currently selected cardset %(cardset)s
 is not compatible with the game
-%s
+%(game)s
 
-Please select a %s type %s.
-''') % (CARDSET, self.cardset.name, gi.name, t[0], CARDSET),
+Please select a %(correct_type)s type cardset.
+''') % {'cardset': self.cardset.name, 'game': gi.name, 'correct_type': t[0]},
             strings=(_("&OK"),), default=0)
         cs = self.__selectCardsetDialog(t)
         if cs is None:
@@ -852,7 +853,7 @@ Please select a %s type %s.
 
     def __selectCardsetDialog(self, t):
         cs = self.selectCardset(
-            _("Please select a %s type %s") % (t[0], CARDSET),
+            _("Please select a %s type cardset") % t[0],
             self.cardset.index)
         return cs
 
@@ -1060,7 +1061,8 @@ Please select a %s type %s.
                 except Exception as ex:
                     if DEBUG:
                         traceback.print_exc()
-                    print_err(_("error loading plugin %s: %s") % (n, ex))
+                    print_err(_("error loading plugin %(file)s: %(err)s") %
+                              {'file': n, 'err': ex})
 
     #
     # init cardsets

pysollib/game/__init__.py
@@ -63,7 +63,7 @@ from pysollib.pysoltk import MfxExceptionDialog, MfxMessageDialog
 from pysollib.pysoltk import after, after_cancel, after_idle
 from pysollib.pysoltk import bind, wm_map
 from pysollib.settings import DEBUG
-from pysollib.settings import PACKAGE, TITLE, TOOLKIT, TOP_TITLE
+from pysollib.settings import PACKAGE, TITLE, TOOLKIT, TOP_SIZE
 from pysollib.settings import VERSION, VERSION_TUPLE
 from pysollib.struct_new import NewStruct
 
@@ -1310,7 +1310,7 @@ class Game(object):
             if not title:
                 title = TITLE
             if not text:
-                text = _("Discard current game ?")
+                text = _("Discard current game?")
             self.playSample("areyousure")
             d = MfxMessageDialog(self.top, title=title, text=text,
                                  bitmap="question",
@@ -1931,19 +1931,24 @@ class Game(object):
                 if ret:
                     if ret[0] and ret[1]:
                         top_msg = _(
-                            '''\nYou have reached\n''' +
-                            '# %d in the %s of playing time' +
-                            '\nand # %d in the %s of moves.') % \
-                            (ret[0], TOP_TITLE, ret[1], TOP_TITLE)
+                            '\nYou have reached\n# %(timerank)d in the top ' +
+                            '%(tops)d of playing time\nand # %(movesrank)d ' +
+                            'in the top %(tops)d of moves.') % {
+                                'timerank': ret[0],
+                                'movesrank': ret[1],
+                                'tops': TOP_SIZE}
                     elif ret[0]:        # playing time
                         top_msg = _(
-                            '''\nYou have reached\n''' +
-                            '''# %d in the %s of playing time.''') \
-                            % (ret[0], TOP_TITLE)
+                            '\nYou have reached\n# %(timerank)d in the top ' +
+                            '%(tops)d of playing time.') % {
+                                'timerank': ret[0],
+                                'tops': TOP_SIZE}
                     elif ret[1]:        # moves
                         top_msg = _(
-                            '''\nYou have reached\n''' +
-                            '# %d in the %s of moves.') % (ret[1], TOP_TITLE)
+                            '\nYou have reached\n# %(movesrank)d in the top ' +
+                            '%(tops)s of moves.') % {
+                                'movesrank': ret[1],
+                                'tops': TOP_SIZE}
                 return top_msg
         elif not demo:
             # only update the session log
@@ -1975,14 +1980,14 @@ class Game(object):
             self.finished = True
             self.playSample("gameperfect", priority=1000)
             self.winAnimation(perfect=1)
-            text = ungettext('''Your playing time is %s\nfor %d move.''',
-                             '''Your playing time is %s\nfor %d moves.''',
+            text = ungettext('Your playing time is %(time)s\nfor %(n)d move.',
+                             'Your playing time is %(time)s\nfor %(n)d moves.',
                              self.moves.index)
-            text = text % (time, self.moves.index)
+            text = text % {'time': time, 'n': self.moves.index}
+            congrats = _('Congratulations, this\nwas a truly perfect game!')
             d = MfxMessageDialog(
                 self.top, title=_("Game won"),
-                text=_('\nCongratulations, this\nwas a truly perfect game !' +
-                       '\n\n%s\n%s\n') % (text, top_msg),
+                text='\n' + congrats + '\n\n' + text + '\n' + top_msg + '\n',
                 strings=(_("&New game"), None, _("&Cancel")),
                 image=self.app.gimages.logos[5])
         elif status == 1:
@@ -1991,16 +1996,14 @@ class Game(object):
             self.finished = True
             self.playSample("gamewon", priority=1000)
             self.winAnimation()
-            text = ungettext('''Your playing time is %s\nfor %d move.''',
-                             '''Your playing time is %s\nfor %d moves.''',
+            text = ungettext('Your playing time is %(time)s\nfor %(n)d move.',
+                             'Your playing time is %(time)s\nfor %(n)d moves.',
                              self.moves.index)
-            text = text % (time, self.moves.index)
+            text = text % {'time': time, 'n': self.moves.index}
+            congrats = _('Congratulations, you did it!')
             d = MfxMessageDialog(
                 self.top, title=_("Game won"),
-                text=(
-                    _('\nCongratulations, you did it !\n\n%s\n%s\n') %
-                    (text, top_msg)
-                ),
+                text='\n' + congrats + '\n\n' + text + '\n' + top_msg + '\n',
                 strings=(_("&New game"), None, _("&Cancel")),
                 image=self.app.gimages.logos[4])
         elif self.gstats.updated < 0:
@@ -2522,7 +2525,8 @@ class Game(object):
                                  '\nGame solved in %d moves.\n',
                                  self.moves.index)
                 text = text % self.moves.index
-                d = MfxMessageDialog(self.top, title=TITLE+_(" Autopilot"),
+                d = MfxMessageDialog(self.top,
+                                     title=_("%s Autopilot") % TITLE,
                                      text=text,
                                      image=self.app.gimages.logos[4],
                                      strings=(s,),
@@ -2536,7 +2540,8 @@ class Game(object):
                 if DEBUG:
                     text += "\nplayer_moves: %d\ndemo_moves: %d\n" % \
                         (self.stats.player_moves, self.stats.demo_moves)
-                d = MfxMessageDialog(self.top, title=TITLE+_(" Autopilot"),
+                d = MfxMessageDialog(self.top,
+                                     title=_("%s Autopilot") % TITLE,
                                      text=text, bitmap=bitmap, strings=(s,),
                                      padx=30, timeout=timeout)
                 status = d.status
@@ -2550,7 +2555,8 @@ class Game(object):
                 s = self.app.miscrandom.choice(
                         (_("&Oh well"), _("&That's life"), _("&Hmm")))
                 # ??? accelerators
-                d = MfxMessageDialog(self.top, title=TITLE+_(" Autopilot"),
+                d = MfxMessageDialog(self.top,
+                                     title=_("%s Autopilot") % TITLE,
                                      text=_("\nThis won't come out...\n"),
                                      bitmap=bitmap, strings=(s,),
                                      padx=30, timeout=timeout)
@@ -2999,7 +3005,7 @@ class Game(object):
         if confirm and self.gsaveinfo.bookmarks.get(n):
             if not self.areYouSure(
                     _("Set bookmark"),
-                    _("Replace existing bookmark %d ?") % (n+1)):
+                    _("Replace existing bookmark %d?") % (n+1)):
                 return 0
         f = BytesIO()
         try:
@@ -3021,7 +3027,7 @@ class Game(object):
             confirm = self.app.opt.confirm
         if confirm:
             if not self.areYouSure(_("Goto bookmark"),
-                                   _("Goto bookmark %d ?") % (n+1)):
+                                   _("Goto bookmark %d?") % (n+1)):
                 return
         try:
             s, moves_index = bm
@@ -3143,9 +3149,9 @@ class Game(object):
         version_tuple = pload(tuple)
         validate(
             version_tuple >= (1, 0),
-            _('''Cannot load games saved with\n%s version %s''') % (
-                PACKAGE,
-                version))
+            _('Cannot load games saved with\n%(app)s version %(ver)s') % {
+                'app': PACKAGE,
+                'ver': version})
         game_version = 1
         bookmark = pload(int)
         validate(0 <= bookmark <= 2, err_txt)

pysollib/games/matriarchy.py
@@ -120,7 +120,8 @@ class Matriarchy_Talon(WasteTalonStack):
             return
         WasteTalonStack.updateText(self, update_rounds=0)
         # t = "Round %d" % self.round
-        t = _("Round %d/%d") % (self.round, self.max_rounds)
+        t = _("Round %(round)d/%(max_rounds)d") % {
+            'round': self.round, 'max_rounds': self.max_rounds}
         self.texts.rounds.config(text=t)
         t = _("Deal %d") % self.DEAL[self.round-1]
         self.texts.misc.config(text=t)

pysollib/help.py
@@ -40,17 +40,18 @@ from pysollib.settings import PACKAGE_URL, TITLE, TOOLKIT, VERSION
 def help_about(app, timeout=0, sound=True):
     if sound:
         app.audio.playSample("about")
-    t = _("A Python Solitaire Game Collection\n")
+    t = _("A Python Solitaire Game Collection")
     if app.miscrandom.random() < 0.8:
-        t = _("A World Domination Project\n")
+        t = _("A World Domination Project")
     strings = (_("&Nice"), _("&Credits..."))
     if timeout:
         strings = (_("&Enjoy"),)
     version = _("Version %s") % VERSION
-    d = PysolAboutDialog(app, app.top, title=_("About ") + TITLE,
+    d = PysolAboutDialog(app, app.top, title=_("About %s") % TITLE,
                          timeout=timeout,
                          text=_('''PySol Fan Club edition
-%s%s
+%(description)s
+%(versioninfo)s
 
 Copyright (C) 1998 - 2003 Markus F.X.J. Oberhumer.
 Copyright (C) 2003 Mt. Hood Playing Card Co.
@@ -60,7 +61,8 @@ All Rights Reserved.
 PySol is free software distributed under the terms
 of the GNU General Public License.
 
-For more information about this application visit''') % (t, version),
+For more information about this application visit''') %
+                         {'description': t, 'versioninfo': version},
                          url=PACKAGE_URL,
                          image=app.gimages.logos[2],
                          strings=strings, default=0,
@@ -86,7 +88,7 @@ def help_credits(app, timeout=0, sound=True):
         t = "kivy"
     d = MfxMessageDialog(
         app.top, title=_("Credits"), timeout=timeout,
-        text=TITLE+_(''' credits go to:
+        text=_('''%(app)s credits go to:
 
 Volker Weidner for getting me into Solitaire
 Guido van Rossum for the initial example program
@@ -95,8 +97,8 @@ Carl Larsson for the background music
 The Gnome AisleRiot team for parts of the documentation
 Natascha
 
-The Python, %s, SDL & Linux crews
-for making this program possible''') % t,
+The Python, %(gui_library)s, SDL & Linux crews
+for making this program possible''') % {'app': TITLE, 'gui_library': t},
         image=app.gimages.logos[3], image_side="right",
         separator=True)
     return d.status
@@ -122,8 +124,8 @@ def help_html(app, document, dir_, top=None):
             document, dir_ = "index.html", "html"
             help_html_index = app.dataloader.findFile(document, dir_)
     except EnvironmentError:
-        MfxMessageDialog(app.top, title=TITLE + _(" HTML Problem"),
-                         text=_("Cannot find help document\n") + document,
+        MfxMessageDialog(app.top, title=_("%s HTML Problem") % TITLE,
+                         text=_("Cannot find help document\n%s") % document,
                          bitmap="warning")
         return None
     # print doc, help_html_index
@@ -136,7 +138,7 @@ def help_html(app, document, dir_, top=None):
         viewer.display(doc, relpath=0)
     except Exception:
         # traceback.print_exc()
-        top = make_help_toplevel(app, title=TITLE+_(" Help"))
+        top = make_help_toplevel(app, title=_("%s Help") % TITLE)
         if top.winfo_screenwidth() < 800 or top.winfo_screenheight() < 600:
             # maximized = 1
             top.wm_minsize(300, 150)

commit 300d5dca7a9d543435567dd9a3850dcc80a0336b

Fix command line option error message
TypeError: unsupported operand type(s) for +: 'GetoptError' and 'str'
caused by commit 65b86f2d1d6f53fef133af897c673568205ce7a3

pysollib/main.py
@@ -93,7 +93,7 @@ def parse_option(argv):
                                        "sound-mod=",
                                        "help"])
     except getopt.GetoptError as err:
-        print_err(err + "\n" + _("try %s --help for more information") %
+        print_err(str(err) + "\n" + _("try %s --help for more information") %
                   prog_name, 0)
         return None
     opts = {"help": False,
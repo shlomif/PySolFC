6016a6a5c136d4d8591ec87a7e5cd6e7162995ee
Juhani Numminen <juhaninumminen0@gmail.com>
2020-03-02 13:21:35 +0200

    No such thing as pysollib.app.comments (#153)

--- pysollib/tk/selectgame.py
+++ pysollib/tk/selectgame.py
@@ -484,7 +484,6 @@ class SelectGameDialogWithPreview(SelectGameDialog):
                 audio=self.app.audio,
                 canvas=canvas,
                 cardset=self.app.cardset.copy(),
-                comments=self.app.comments.new(),
                 gamerandom=self.app.gamerandom,
                 gdb=self.app.gdb,
                 gimages=self.app.gimages,

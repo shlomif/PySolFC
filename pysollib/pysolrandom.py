c4545ab1145c57fe4b262967c3632ee1a8fb5102
Shlomi Fish <shlomif@shlomifish.org>
2020-05-25 11:49:15 +0300

    extract more code to pysol_cards

--- pysollib/pysolrandom.py
+++ pysollib/pysolrandom.py
@@ -26,7 +26,7 @@
 import re
 
 import pysol_cards
-assert getattr(pysol_cards, 'VERSION', (0, 0, 0)) >= (0, 8, 11), (
+assert getattr(pysol_cards, 'VERSION', (0, 0, 0)) >= (0, 8, 12), (
     "Newer version of https://pypi.org/project/pysol-cards is required.")
 import pysol_cards.random  # noqa: I100
 import pysol_cards.random_base  # noqa: I100
@@ -51,16 +51,6 @@ class MFXRandom(pysol_cards.random.PysolRandom):
 
     def setstate(self, state):
         self.seed = state
-
-    #
-    # implementation
-    #
-
-    def choice(self, seq):
-        return seq[int(self.random() * len(seq))]
-
-    def randrange(self, a, b):
-        return self.randint(a, b-1)
 
 
 # ************************************************************************

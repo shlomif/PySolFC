2b4f59369a8f84c126d125d3e8947e6ed034b6e1
Shlomi Fish <shlomif@shlomifish.org>
2020-06-12 07:07:31 +0300

    fix flake8 tests

--- pysollib/game/dump.py
+++ pysollib/game/dump.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 from pysol_cards.random import random__str2int
+
 from pysollib.settings import PACKAGE
 from pysollib.settings import VERSION, VERSION_TUPLE
 

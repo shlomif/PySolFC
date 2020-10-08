da5b85c064d28a5bf987d52d2c5003952f87d55e
Juhani Numminen <juhaninumminen0@gmail.com>
2020-10-08 15:10:22 +0300

    contrib/install-pysolfc.sh: pip install pygame for sound output (#180)

--- contrib/install-pysolfc.sh
+++ contrib/install-pysolfc.sh
@@ -2,15 +2,15 @@
 set -Cefu
 
 : ${PKGTREE:=/usr/local/packages/PySolFC}
-PIP=("${PKGTREE}/env/bin/pip" install --no-binary :all:)
-PYPROG="$(printf '%s/env/bin/python' "$PKGTREE")"
+PIP="${PKGTREE}/env/bin/pip"
+PYPROG="${PKGTREE}/env/bin/python"
 VERSION="$(env PYTHONPATH=`pwd` "$PYPROG" -c 'from pysollib.settings import VERSION ; print(VERSION)')"
-XZBALL="$(printf 'dist/PySolFC-%s.tar.xz' "$VERSION")"
-reqs='six random2 pillow'
+XZBALL="dist/PySolFC-${VERSION}.tar.xz"
+reqs=(pillow pygame random2 six)
 
 make dist
-for req in $reqs
-do
-    "${PIP[@]}" "$req"
-done
-"${PIP[@]}" --upgrade "$XZBALL"
+
+"$PIP" install wheel
+"$PIP" install "${reqs[@]}"
+
+"$PIP" install --upgrade "$XZBALL"

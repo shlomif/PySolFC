701c1628d520602052596247d8b2ef4f56c0377d
Joe R <joeraz5@verizon.net>
2022-08-18 21:16:40 -0400

    Find card support for Ganjifa games.

--- pysollib/game/__init__.py
+++ pysollib/game/__init__.py
@@ -3439,11 +3439,7 @@ class Game(object):
 
     # for find_card_dialog
     def canFindCard(self):
-        return (self.gameinfo.category == GI.GC_FRENCH or
-                self.gameinfo.category == GI.GC_HANAFUDA or
-                self.gameinfo.category == GI.GC_TAROCK or
-                self.gameinfo.category == GI.GC_HEXADECK or
-                self.gameinfo.category == GI.GC_MAHJONGG)
+        return self.gameinfo.category != GI.GC_TRUMP_ONLY
 
     #
     # subclass hooks

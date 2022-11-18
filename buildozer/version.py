86bf23c1fa4ca0208ef27c4c1050838fa92c3285
lufebe16 <lufebe16@bluewin.ch>
2022-11-18 16:05:09 +0100

    Build support for android reworked: - New build support for android is based on buildozer. - Support files and instructions see directory 'buildozer'

---
+++ buildozer/version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# ---------------------------------------------------------------------------
+
+import sys
+sys.path.insert(0, str(".."))
+
+from pysollib.settings import VERSION
+print ("VERSION =",VERSION)

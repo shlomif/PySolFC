e858b38de17798fca5274e5723b47c60a45479f5
Joe R <joeraz5@verizon.net>
2023-10-02 17:24:51 -0400

    Added Yukon Cells game.

--- pysollib/gamedb.py
+++ pysollib/gamedb.py
@@ -576,7 +576,7 @@ class GI:
         ('fc-2.20', tuple(range(855, 897))),
         ('fc-2.21', tuple(range(897, 900)) + tuple(range(11014, 11017)) +
          tuple(range(13160, 13163)) + (16682,)),
-        ('dev', tuple(range(906, 925)) + tuple(range(11017, 11020)) +
+        ('dev', tuple(range(906, 926)) + tuple(range(11017, 11020)) +
          tuple(range(22303, 22311)) + tuple(range(22353, 22361))),
     )
 
